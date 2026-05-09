53db0b3b0fcee7dc6c737d4f2d8d7da3b3aebdf8
Pat9990 <patikuk@onet.pl>
2026-05-09 15:00:51 +0200

    feed animals done

--- app/main.py
+++ app/main.py
@@ -14,15 +14,13 @@ class Animal:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
             return self.appetite
-        else:
-            return 0
+        return 0
 
 
 class Cat(Animal):
 
-    def __init__(self, name: str, appetite: int = 3,
-                 is_hungry: bool = True) -> None:
-        super().__init__(name, appetite, is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 3, is_hungry)
 
     def catch_mouse(self) -> None:
         print("The hunt began!")
@@ -30,16 +28,12 @@ class Cat(Animal):
 
 class Dog(Animal):
 
-    def __init__(self, name: str, appetite: int = 7,
-                 is_hungry: bool = True) -> None:
-        super().__init__(name, appetite, is_hungry)
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 7, is_hungry)
 
     def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
 
 def feed_animals(animals: list[Animal]) -> int:
-    suma = 0
-    for animal in animals:
-        suma += animal.feed()
-    return suma
+    return sum(animal.feed() for animal in animals)
